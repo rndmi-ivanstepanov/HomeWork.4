6975430e39a838f80b30c2f958c0ab15ae37b940
Rndmi <SireOfAll79@yandex.ru>
2023-02-15 15:08:10 +0300

    Initial commit

---
+++ src/IfElseSwitch.java
@@ -0,0 +1,10 @@
+public class IfElseSwitch {
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    public static void task1() {
+        System.out.println("123");
+    }
+}
